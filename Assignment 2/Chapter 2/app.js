db487d12aed0aac95a0959cd15336b67640dbf4a
MoosaVayani <121932940+MoosaVayani@users.noreply.github.com>
2023-02-09 21:16:20 +0500

    push 3

---
+++ Assignment 2/Chapter 2/app.js
@@ -0,0 +1,36 @@
+var username
+
+
+var myname 
+
+
+
+var message =("Hello World")
+
+alert (message)
+
+
+var studentName = "Moosa Vayani"
+alert (studentName)
+var studentAge = "28 Year Old"
+alert (studentAge)
+var studentSkills = "Mobile Software Technician"
+alert (studentSkills)
+
+var pizza ="PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert (pizza)
+
+
+var email = "my email address is moosa.vayani@gmail.com"
+
+alert (email)
+
+var book = "i'm Trying To Learn From The Book \n A smarter way to learn JavaScript."
+alert(book)
+
+var display = "Yah! I can write HTML content through JavaScript"
+document.write  (display)
+
+var display1 = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert  (display1)
+document.write  (display1)
---
+++ Assignment 2/Chapter 3/app.js
@@ -0,0 +1,32 @@
+// Chapter 3
+// VARIABLES FOR NUMBERS
+
+
+//Question 1
+var age = 28
+
+
+alert ("i am " + age + " years old")
+
+
+//Question 2
+ 
+var n = 14
+
+alert ("You have visited this site " + n + " times" )
+
+//Question 3
+
+var birthYear = 1994
+
+document.write ("My Birth Year is " + birthYear + " <br>  Data Type of my Declared Variable is Number. ")
+
+
+//Question 4
+
+var name = "Moosa";
+var title = 5;
+var store = "levis";
+document.write( "<br>" + name + " ordered " +title +" T-shirts on " +store +" Store.")
+
+
